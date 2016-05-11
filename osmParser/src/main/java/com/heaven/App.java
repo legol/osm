c18f83d0be59c3d36b9ed22ab20b58143fd070eb
chenjie3 <chenjie3@kingsoft.com>
2016-05-11 13:32:29 +0800

    add log4j

--- osmParser/src/main/java/com/heaven/App.java
+++ osmParser/src/main/java/com/heaven/App.java
@@ -1,13 +1,19 @@
 package com.heaven;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+
+
     public static void main( String[] args )
     {
+        LOGGER.info("app launch.");
         System.out.println( "Hello World!" );
     }
 }
