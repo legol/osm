78dffc93e15a987c463dad8dbac4703324452b00
legol <legol.chen@gmail.com>
2016-05-13 21:44:21 +0800

    sample code of return image to browser.

--- osmImageGenerator/src/main/java/com/heaven/osm/imagegenerator/controller/ImageGenerator.java
+++ osmImageGenerator/src/main/java/com/heaven/osm/imagegenerator/controller/ImageGenerator.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 /**
@@ -23,4 +29,40 @@ public class ImageGenerator {
         return "hello osm image generator";
     }
 
+    @RequestMapping(value = "/getImage")
+    public void getImage(HttpServletRequest request, HttpServletResponse response){
+
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream inputStream = loader.getResourceAsStream("test.jpg");
+            BufferedImage bufferedImage = ImageIO.read(inputStream);
+            ImageIO.write(bufferedImage, "jpeg", jpegOutputStream);
+        } catch (IllegalArgumentException e) {
+            try {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] imgByte = jpegOutputStream.toByteArray();
+
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream = null;
+        try {
+            responseOutputStream = response.getOutputStream();
+            responseOutputStream.write(imgByte);
+            responseOutputStream.flush();
+            responseOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
