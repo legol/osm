1b5b7136e20d67f77a620f6ebeb1e7c69a461c4e
legol <legol.chen@gmail.com>
2016-06-30 23:14:10 +0800

    osm frontent with precompiled jsx and react

--- osmFrontend2/components/main.jsx
+++ osmFrontend2/components/main.jsx
@@ -1,4 +1,4 @@
-var ExampleApplication = React.createClass({
+var Application = React.createClass({
   render: function() {
     var elapsed = Math.round(this.props.elapsed  / 100);
     var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
@@ -13,7 +13,7 @@ var start = new Date().getTime();
 
 setInterval(function() {
   ReactDOM.render(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
+    <Application elapsed={new Date().getTime() - start} />,
     document.getElementById('container')
   );
 }, 50);
