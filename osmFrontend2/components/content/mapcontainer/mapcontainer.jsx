6a9df884f90166c9a9309896584937b9c82dc874
chenjie3 <chenjie3@kingsoft.com>
2016-07-21 15:01:28 +0800

    add tile_controller.js and tile.js

--- osmFrontend2/components/content/mapcontainer/mapcontainer.jsx
+++ osmFrontend2/components/content/mapcontainer/mapcontainer.jsx
@@ -1,16 +1,31 @@
 /**
  * Created by legol on 6/30/2016.
  */
+
 var mapContainerStyle = {
     height:'100%',
     width:'100%',
     border: '2px solid #0000FF',
 };
 
+var mapCanvasStyle = {
+    height:'500px',
+    width: '500px',
+    border: '1px solid #FF0000',
+    borderRadius: '0px',
+    position: 'absolute',
+    margin:0,
+    padding:0,
+};
+
 var MapContainer = React.createClass({
     render: function() {
         return <div id='map_container' style={mapContainerStyle}>
-            map goes here.
+            <div id="map_canvas" style={mapCanvasStyle}></div>
         </div>;
+    },
+
+    componentDidMount: function(){
+        window.tileController.init();
     }
 });
